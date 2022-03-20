b36f3debcd81c534a483b566db126275e624ac64
Esteban Pinto L <estebanpinto1284@gmail.com>
2022-03-20 16:07:31 -0700

    Second step Difining string and trying to match the pattern

--- origin_spp.py
+++ origin_spp.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python3
 
 
+import re
 
+"A script to match specific patterns (words) in a text and print them and the number of lines where they appear" 
+
+re_patter_string = r'[inherit....\w*]'
+re_patter_object = re.compile(re_patter_string, re.MULTILINE)
+#re_patter_object.findall(re_patter_string)
+
+#print (inherit)
 
 
 print('Opening origin.txt')
@@ -11,9 +19,11 @@ with open('origin.txt', 'r') as in_stream:
         for line in in_stream:
             line = line.strip()
             inherit_list = line.split()
-            inherit_list.sort()
+            re_patter_object.findall(re_patter_string)
+            #inherit_list.sort()
             for word in inherit_list:
                 out_stream.write('{0}\n'.format(word))
-print("Done!")
-print('origin.txt is closed?', in_stream.closed)
-print('origin_spp.txt is closed?', out_stream.closed)
+            
+#print("Done!")
+#print('origin.txt is closed?', in_stream.closed)
+#print('origin_spp.txt is closed?', out_stream.closed)
