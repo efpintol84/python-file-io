17735b8c42287f7f0fad7ef5b5f5b2da0f0c2373
Esteban Pinto L <estebanpinto1284@gmail.com>
2022-03-21 10:53:08 -0700

    I got something

--- origin_spp.py
+++ origin_spp.py
@@ -5,8 +5,8 @@ import re
 
 "A script to match specific patterns (words) in a text and print them and the number of lines where they appear" 
 
-re_pattern_string = r'^(inherit|INHERIT|Inherti)*\w'
-re_pattern_object = re.compile(re_pattern_string)
+#re_pattern_string = r'^(?:inherit|INHERIT|Inherti)*\w'
+#re_pattern_object = re.compile(re_pattern_string)
 #to_search = re_pattern_object.findall('origin.txt')
 #print(to_search)
 
@@ -17,14 +17,18 @@ with open('origin.txt', 'r') as in_stream:
         for line in in_stream:
             line = line.strip()
             inherit_list = line.split()
-            to_search = re_pattern_object.findall('origin.txt')
-            print(to_search)
+            #to_search = re_pattern_object.findall('origin.txt')
+            #print(to_search)
             #inherit_list.sort()
-           # for word in inherit_list:
-           #     out_stream.write('{0}\n'.format(word))
+            for word in inherit_list:
+                out_stream.write('{0}\n'.format(word))
 
 print("Done!")
 print('origin.txt is closed?', in_stream.closed)
 print('origin_spp.txt is closed?', out_stream.closed)
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    re_pattern_string = r'^(?:inherit|INHERIT|Inherti)*\w'
+    re_pattern_object = re.compile(re_pattern_string)
+    to_search = re_pattern_object.findall('origin.txt')
+    print (to_search)
