0b0c600bc3c2683c8911eb137a9cab8d6cefecdd
Esteban Pinto L <estebanpinto1284@gmail.com>
2022-03-22 22:23:34 -0700

    Adding informative docstrings

--- origin_spp.py
+++ origin_spp.py
@@ -5,27 +5,48 @@ import re
 
 "A script to match specific patterns (words) in a text and print them and the number of lines where they appear" 
 
-re_pattern_string = r'(?:inherit|INHERIT|Inherti)*\w'
-re_pattern_object = re.compile(re_pattern_string)
-#to_search = re_pattern_object.findall('origin.txt')
+
+"""
+
+First, I define the string pattern that I need to find in the origin.txt file" 
+
+"""
+re_pattern_string = r'(?:inherit|INHERIT|Inherit)*\w'
+
+
+
+"""
+
+Next, step I open the file and seek line by line the term/s defined in the previous step
+
+Requierements
+_____________
+
+1. It is importand to use with when opening the document to ensure it will be closed when I
+run the script.
+
+2. I will generate an additional file (origin_spp.txt) where I will keep all the words that
+I requiered
+
+"""
+
 
 print('Opening origin.txt')
 with open('origin.txt', 'r') as in_stream:
     print('Opening origin_spp.txt')
     with open('origin_spp.txt', 'w') as out_stream:
-        for line in in_stream:
+        for num, line in enumerate (in_stream):
+            re_pattern_object = re.compile(re_pattern_string)
             line = line.strip()
             inherit_list = line.split()
-            origin_list = re_pattern_object.findall('inherit_list')
-            origin_list = inherit_list.append('origin_list')
-	    #print(origin_list)
-            #inherit_list.sort()
-            #for word in to_search:
-               # out_stream.write('{0}\n'.format(word))
+            temp_list = re_pattern_object.findall('line')
+            complete = origin_list.append('temp_list')
+            for word in complete:
+                out_stream.write(str(num) + '\t{0}\n'.format(word))
 
 print("Done!")
 print('origin.txt is closed?', in_stream.closed)
 print('origin_spp.txt is closed?', out_stream.closed)
 
 if __name__ == '__main__':
-    print (origin_list)
+    print(origin_list)
