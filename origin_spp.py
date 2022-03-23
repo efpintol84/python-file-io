a7aeab2080a1429019e602b6c291074c654d9d75
Esteban Pinto L <estebanpinto1284@gmail.com>
2022-03-22 19:25:07 -0700

    new oroginList

--- origin_spp.py
+++ origin_spp.py
@@ -5,10 +5,9 @@ import re
 
 "A script to match specific patterns (words) in a text and print them and the number of lines where they appear" 
 
-#re_pattern_string = r'^(?:inherit|INHERIT|Inherti)*\w'
-#re_pattern_object = re.compile(re_pattern_string)
+re_pattern_string = r'(?:inherit|INHERIT|Inherti)*\w'
+re_pattern_object = re.compile(re_pattern_string)
 #to_search = re_pattern_object.findall('origin.txt')
-#print(to_search)
 
 print('Opening origin.txt')
 with open('origin.txt', 'r') as in_stream:
@@ -17,18 +16,16 @@ with open('origin.txt', 'r') as in_stream:
         for line in in_stream:
             line = line.strip()
             inherit_list = line.split()
-            #to_search = re_pattern_object.findall('origin.txt')
-            #print(to_search)
+            origin_list = re_pattern_object.findall('inherit_list')
+            origin_list = inherit_list.append('origin_list')
+	    #print(origin_list)
             #inherit_list.sort()
-            for word in inherit_list:
-                out_stream.write('{0}\n'.format(word))
+            #for word in to_search:
+               # out_stream.write('{0}\n'.format(word))
 
 print("Done!")
 print('origin.txt is closed?', in_stream.closed)
 print('origin_spp.txt is closed?', out_stream.closed)
 
 if __name__ == '__main__':
-    re_pattern_string = r'^(?:inherit|INHERIT|Inherti)*\w'
-    re_pattern_object = re.compile(re_pattern_string)
-    to_search = re_pattern_object.findall('origin.txt')
-    print (to_search)
+    print (origin_list)
